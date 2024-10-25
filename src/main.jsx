50aa988085015108c2cd16926f527d8df4631af3
AnastasiiaTitarenko <151573636+AnastasiiaTitarenko@users.noreply.github.com>
2024-10-26 00:45:03 +0300

    add formik

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import ReactDOM from "react-dom/client";
+import App from "./components/App/App";
+import { Provider } from "react-redux";
+
+import { store } from "./redux/store";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+   
     <App />
-  </StrictMode>,
-)
+  
+  </Provider>
+);
